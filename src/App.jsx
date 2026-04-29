e46acd42073d4990d41937ec88ccad3c1b070b71
Vedika Jain <vedikajain.350@gmail.com>
2026-04-29 16:47:50 -0700

    updated text

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <main className="app">
       <h1>Hello from my live project!</h1>
-      <button type="button">I shipped this.</button>
+      <button type="button">I shipped this and I'm badASS.</button>
     </main>
   )
 }
